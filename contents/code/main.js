d9b89af189923681f549e30203decb596fddaf42
Moritz Schmidt <ms@plista.com>
2013-02-07 17:25:52 +0100

    working ugly version. that api i work against is stupid & will likely change.

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,38 +1,56 @@
-var layout = new LinearLayout(plasmoid);
+var
+	layout = new LinearLayout(plasmoid),
+	label  = new Label(),
+	refreshView = function (dataString) {
+		var
+			lines = [],
+			statusMap = {
+				1: 'OK',
+				2: 'WARN',
+				3: 'ERROR',
+				4: 'UNKNOWN'
+			};
+		Array.prototype.forEach.call(dataString, function (character, idx) {
+			// filter ctrl chars
+			if ('<>'.indexOf(character) !== -1) {
+				return;
+			}
+			lines.push('p' + (idx + 100) + ': ' + (statusMap[character] || 'WTF'));
+		});
+		label.text = lines.join('\n');
+	},
+	getData = function () {
 
-var label = new Label();
+		var
+			httpJob = plasmoid.getUrl("http://nagios.plista.com/map/nag.php"),
+			responseBody = '';
+
+		print('refreshing...');
+
+		httpJob.data.connect(function (job, data) {
+
+			if (job != httpJob) {
+				return;
+			}
+
+			if (!data.length) {
+				return;
+			}
+			responseBody += data.toUtf8();
+		});
+
+		httpJob.finished.connect(function (job) {
+			if (job != httpJob) {
+				return;
+			}
+			print('got response: ' + responseBody + '. refreshing view...');
+			refreshView(responseBody);
+		});
+	},
+	timer = new QTimer();
+
 layout.addItem(label);
 
-label.text = '';
-;
-/*
-for (var n in plasmoid) {
-	names.push(n);
-}
-
-names = names.sort();
-
-label.text = names.join('\n');
-
-
-label.text = plasmoid.getOwnPropertyNames().join(',');
-
- */
-var httpJob = plasmoid.getUrl("http://nagios.plista.com/map/nag.php");
-
-httpJob.data.connect(function (job, data) {
-	if (job == httpJob) {
-		print("we have our job");
-		if (data.length) {
-			label.text += data.toUtf8();
-			output.append(data.toUtf8())
-		}
-	}
-});
-
-httpJob.finished.connect(function (job) {
-	if (job == httpJob) {
-		print("ende");
-	}
-	label.text
-});
+getData();
+timer.timeout.connect(getData);
+timer.start(5000);
